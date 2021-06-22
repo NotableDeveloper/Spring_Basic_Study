42a1ebe33896ae6e0eb95bb3b8f2de2f91ff1141
NotableDeveloper <castlerea@naver.com>
2021-06-23 00:32:15 +0900

    Spring Bean을 컨테이너에 등록

--- core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -3,13 +3,20 @@ package hello.core.discount;
 import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class RateDiscountPolicyTest {
-    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+    DiscountPolicy discountPolicy;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+         discountPolicy = appConfig.discountPolicy();
+    }
 
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
